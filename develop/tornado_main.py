8e0700673091e68bff31e85cb53825d2b94dae30
palloc <palloc.mknet10@gmail.com>
2016-09-09 14:35:45 +0900

    add bid handler

--- develop/tornado_main.py
+++ develop/tornado_main.py
@@ -1,14 +1,19 @@
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
+import tornado.httpserver
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("test")
-
+class BidHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("test bid request.")
+        
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/", MainHandler),
     ])
-
-    application.listen(8080)
+    
+    application.listen(80)
     tornado.ioloop.IOLoop.current().start()
