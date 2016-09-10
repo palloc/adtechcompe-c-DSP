76e645ae6dd983b3f829055e7525698395dad120
palloc <palloc.mknet10@gmail.com>
2016-09-10 17:52:53 +0900

    fixed setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,5 @@
 yum install -y epel-release
 yum install -y python-pip
 yum install -y gcc gcc-c++ openssl-devel
-pip install -y pandas
-pip install -y tornado aerospike
+yes | pip install pandas
+yes | pip install tornado aerospike
