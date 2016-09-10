cce21198041a27f28bf88eb83dbe63eb20837e52
palloc <palloc.mknet10@gmail.com>
2016-09-11 01:49:36 +0900

    add some

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,3 +4,6 @@ yum install -y python-pip
 yum install -y gcc gcc-c++ openssl-devel
 yes | pip install pandas
 yes | pip install tornado aerospike
+yes | pip install --upgrade pip
+yes | pip install scipy
+yes | pip install sklearn
